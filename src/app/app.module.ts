968edb71c6b6b28cb3120cccb2610877790dbde1
Aayush Kapoor <xeoneux@icloud.com>
2018-10-06 02:10:46 +0530

    Prepare scripts for heroku release

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,16 @@ import { AppController } from "./app.controller";
 
 @Module({
   controllers: [AppController],
-  imports: [TypeOrmModule.forRoot(), UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      entities: [__dirname + "/../**/*.entity.ts"],
+      synchronize: true,
+      type: "postgres",
+      url:
+        process.env.DATABASE_URL || "postgres://root:root@localhost:5432/test"
+    }),
+    UserModule
+  ],
   providers: []
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { config } from "dotenv";
+import { resolve } from "path";
 
 import { AppModule } from "./app/app.module";
 
 config();
 
 (async () => {
+  const port = process.env.PORT || 5678;
   const app = await NestFactory.create(AppModule);
+  app.useStaticAssets(resolve(__dirname, "../build"));
 
   const options = new DocumentBuilder()
     .setTitle("Vyng Backend Challenge")
@@ -17,5 +20,5 @@ config();
 
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup("api", app, document);
-  await app.listen(5678);
+  await app.listen(port);
 })();
